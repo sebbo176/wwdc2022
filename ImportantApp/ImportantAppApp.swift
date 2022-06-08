a85afc46a300e706ffe576b4ef537a7aab50e532
Sebastian Bolling <sebastian.bolling@bookbeat.com>
2022-06-08 11:34:11 +0200

    Initial Commit

---
+++ ImportantApp/ImportantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImportantAppApp.swift
+//  ImportantApp
+//
+//  Created by Sebastian Bolling on 2022-06-08.
+//
+
+import SwiftUI
+
+@main
+struct ImportantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
